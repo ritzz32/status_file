291183d8b7fc17fa3fffe4939b8483ef3c69e3d9
tgeorge <tgeorge@clearwateranalytics.com>
2017-10-16 14:34:55 -0600

    Add template for initial class

---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+# status_file
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include status_file
+class status_file {
+}
